41206f3b01a4f9482658e5eb051523dd9dc08f77
traviicii <125103256+traviicii@users.noreply.github.com>
2024-06-12 20:18:22 -0400

    initial commit

---
+++ 1_regex.py
@@ -0,0 +1,12 @@
+# Regex
+
+# Usecases
+
+#-- Searching text data for data of a particular pattern
+#-- Validating data
+#-- Manipulate text
+#-- Build patterns to analyze and manipulate text
+
+# Terminology
+# Literal characters: exact characters. a, b, c, d, ""
+# Metacharacters: special symbols that represent broader categories
